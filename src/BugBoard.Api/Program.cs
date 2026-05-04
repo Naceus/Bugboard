a24079f170d65e1f04954ae7756bd2a2addd7d1c
Naceus <naco1606@gmail.com>
2026-05-04 18:51:20 +0200

    Add initial database migration

--- src/BugBoard.Api/Program.cs
+++ src/BugBoard.Api/Program.cs
@@ -1,8 +1,12 @@
+using BugBoard.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<BugBoardDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
